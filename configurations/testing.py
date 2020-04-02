4168c5ea3758740a05f4a877e6d3ec0aea07a8da
xSilv4r <chedi.chered@edu.isetcom.tn>
2020-04-02 15:28:36 +0200

    nmap multi processing done and menu fixed.

--- configurations/testing.py
+++
@@ -1,6 +0,0 @@
-with open('multitest.conf', 'r') as f:
-    for line in f:
-        if line.find("output/recon_output/") != -1:
-            b = line.rpartition('/')
-            print(b[2].strip())
-            
